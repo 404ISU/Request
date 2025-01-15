bebf937152d0db4728f5a99b73e0a66ea3babdfb
404ISU <112893915+404ISU@users.noreply.github.com>
2025-01-15 06:00:17 +0300

    Add files via upload

--- server/models/Request.js
+++ server/models/Request.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const requestSchema = new mongoose.Schema({
-    url: String,
-    method: String,
-    headers: Object,
-    timestamp: { type: Date, default: Date.now }
+  url: String,
+  method: String,
+  headers: Object,
+  body: Object, // Новое поле для хранения тела запроса
+  timestamp: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Request', requestSchema);
